b24baa5254e08b24169b7d425c48a05002ff2160
Franco RATOVOSON <jean.franco.ratovoson@gmail.com>
2025-05-31 22:07:54 +0300

    feat(front): add redirect

--- apps/storefront/src/app/(app)/component.tsx
+++ apps/storefront/src/app/(app)/component.tsx
@@ -9,20 +9,30 @@ import { toast } from 'sonner'
 import { useLocalStorage } from '@uidotdev/usehooks'
 
 export default function LayoutComponent({ children }: React.PropsWithChildren) {
-  const [username] = useLocalStorage('username')
+  const [username] = useLocalStorage('username', null)
 
   const router = useRouter()
   const { mutate } = useMutation({
     mutationFn: logout,
-    onSuccess: () => router.push('/home'),
+    onSuccess: () => {
+      localStorage.clear()
+      router.push('/')
+    },
     onError: err => {
       console.error('Error on logout :', err)
       toast(`Error when loging out: ${err.message}`)
     }
   })
 
+  if (!username) {
+    localStorage.clear()
+    router.push('/')
+    toast('You need to log in to navigate this  page')
+    return null
+  }
+
   return (
-    <AppLayout onLogout={mutate} username={(username as string) ?? ''}>
+    <AppLayout onLogout={mutate} username={username}>
       {children}
     </AppLayout>
   )
